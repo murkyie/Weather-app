04786f4366e69e2a7bdfc313cdc354333e2fc61f
murkyie <omarbengadha02@gmail.com>
2022-12-22 00:31:09 +0100

    first commit

---
+++ src/components/TimeAndLocation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { formatToLocalTime } from '../services/weatherService'
+
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+            <p className="text-white text-xl font-light">{formatToLocalTime(dt, timezone)}</p>
+        </div>
+        <div className="flex items-center justify-center my-6">
+            <p className="text-white text-2xl font-medium">{`${name}, ${country}`}</p>
+        </div>
+    </div>  
+  )
+}
+
+export default TimeAndLocation
